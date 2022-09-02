cb6ce0058d034a2e7db5148529e7232445902897
Zohreh Bodaghi <zohreh_bodaghi73@yahoo.com>
2022-09-02 19:14:05 +0430

    create new route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages & componnents
+import Main from "./pages/main";
+
+//styles
 import './App.scss';
 
 function App() {
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
