50add4227cd0f7e3e7c407cd7e1733eede83562d
Zohreh Bodaghi <zohreh_bodaghi73@yahoo.com>
2022-09-06 03:49:58 +0430

    changes subscription component

--- src/components/subscription/subscription.js
+++ src/components/subscription/subscription.js
@@ -4,19 +4,17 @@ import './subscription.scss';
 function Subscription() {
     return (
         <div className='subscription'>
-            <div className='subscription__caption'>
-                <div className='subscription__caption--titr'>
-                    <span>با خرید اشتراک میتونی:</span>
-                </div>
-                <div className='subscription__caption--items'>
-                    <span>بیشتر از 2 مقصد برای مسافرت مشخص کنی.</span>
-                    <span>مدت اقامت در مقصد های مختلف رو تعیین کنی.</span>
-                    <span>میتونیم 3 نوع برنامه متنوع برای سفرت مشخص کنیم</span>
-                </div>
+            <div className='subscription__title'>
+                <span>با خرید اشتراک میتونی:</span>
             </div>
-            <div className='subscription__button'>
-                <button>
-                    <a href="/authentication">خرید اشتراک</a>
+            <div className='subscription__items'>
+                <span className='subscription__item'>بیشتر از 2 مقصد برای مسافرت مشخص کنی.</span>
+                <span className='subscription__item'>مدت اقامت در مقصد های مختلف رو تعیین کنی.</span>
+                <span className='subscription__item'>میتونیم 3 نوع برنامه متنوع برای سفرت مشخص کنیم</span>
+            </div>
+            <div className='subscription__buy'>
+                <button className='subscription__button'>
+                    <a className='subscription__link' href="/authentication">خرید اشتراک</a>
                 </button>
             </div>
         </div>
